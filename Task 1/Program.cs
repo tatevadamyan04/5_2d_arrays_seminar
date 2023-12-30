1b88fac8dcb077d2b2bc589d6d65144bd7b1e613
Tatevik Adamyan <tatevadamyan2004@gmail.com>
2023-12-30 21:57:29 +0400

    Task  1 is done

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+            };
+
+Console.WriteLine("Enter the row index: ");
+int rowIndex = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the column index: ");
+int colIndex = int.Parse(Console.ReadLine());
+
+int result = GetElementValue(array, rowIndex, colIndex);
+
+if (result != int.MinValue)
+    Console.WriteLine($"The element at position ({rowIndex}, {colIndex}) is: {result}");
+else
+    Console.WriteLine($"Element at position ({rowIndex}, {colIndex}) does not exist.");
+
+static int GetElementValue(int[,] array, int row, int col)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+
+    if (row >= 0 && row < rows && col >= 0 && col < cols)
+        return array[row, col];
+    else
+        return int.MinValue;
+}
